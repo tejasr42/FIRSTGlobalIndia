b9eb7d0f964082d203144833299c52a4a706129f
Tejas Ramdas <tejas.ramdas@gmail.com>
2017-06-09 08:57:40 +0530

    DistanceSensor working

--- FirstOpMode.java
+++ FirstOpMode.java
@@ -4,9 +4,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp(name = "Test5", group = "Practice Mode")
 public class FirstOpMode extends LinearOpMode {
@@ -14,11 +18,13 @@ public class FirstOpMode extends LinearOpMode {
     private DcMotor leftfront, rightfront, leftback, rightback, collector, lift;
     private Servo sorter;
     private ColorSensor colorsort;
+    private DistanceSensor prox;
     private ElapsedTime period = new ElapsedTime();
 
 
 
     public void runOpMode(){
+        double left,right,sig;
         leftfront = hardwareMap.dcMotor.get("left_front");
         rightfront = hardwareMap.dcMotor.get("right_front");
         leftback = hardwareMap.dcMotor.get("left_back");
@@ -27,26 +33,56 @@ public class FirstOpMode extends LinearOpMode {
         lift=hardwareMap.dcMotor.get("lift");
         sorter=hardwareMap.servo.get("sorter");
         colorsort=hardwareMap.colorSensor.get("color");
-
+        prox = (DistanceSensor)hardwareMap.get("color");
+        leftfront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftback.setDirection(DcMotorSimple.Direction.REVERSE);
         resetComponents();
 
         waitForStart();
         while(opModeIsActive()){
-            leftfront.setPower(-gamepad1.left_stick_y);
-            rightfront.setPower(-gamepad1.rightt_stick_y);
-            leftback.setPower(-gamepad1.left_stick_y);
-            rightback.setPower(-gamepad1.right_stick_y);
-            collector.setPower(gamepad1.left_trigger);
-            lift.setPower(gamepad1.right_trigger);
-
-            sorter.setPosition(gamepad2.left_stick_y);
-
+            left=-gamepad1.left_stick_y;
+            right=-gamepad1.left_stick_y;
+            sig=Math.signum(left);
+            if(sig == 0){
+                sig=1;
+            }
+            left+=sig*gamepad1.left_stick_x;
+            right-=sig*gamepad1.left_stick_x;
+            if(Math.max(Math.abs(left),Math.abs(right))>1) {
+                left /= Math.max(Math.abs(left),Math.abs(right));
+                right /= Math.max(Math.abs(left),Math.abs(right));
+            }
+//            if(gamepad1.right_stick_x>0.2){
+//                left=gamepad1.right_stick_x;
+//                right=-gamepad1.right_stick_x;
+//            }
+            leftfront.setPower(left);
+            rightfront.setPower(right);
+            leftback.setPower(left);
+            rightback.setPower(right);
+            collector.setPower(0.8);
+            lift.setPower(0.65);
+            if(prox.getDistance(DistanceUnit.CM)<6 && colorsort.blue()<colorsort.red()){
+                waitForTick(150);
+                sorter.setPosition(0.867);
+                waitForTick(400);
+                sorter.setPosition(0.6);
+            }
+            //sorter.setPosition(0.5+0.36*gamepad2.left_stick_y);
+            /*telemetry.addData("prox",prox.getDistance(DistanceUnit.CM));
+            telemetry.addData("color",colorsort.blue());
+            telemetry.update();*/
             boolean is = waitForTick(40);
             if(!is) return;
         }
         resetComponents();
 
     }
+
+    private void sortBalls(){
+
+    }
+
     private void resetComponents(){
         leftfront.setPower(0);
         rightfront.setPower(0);
@@ -54,6 +90,7 @@ public class FirstOpMode extends LinearOpMode {
         rightback.setPower(0);
         collector.setPower(0);
         lift.setPower(0);
+        sorter.setPosition(0.6);
     }
     private boolean waitForTick(long periodMs){
         try{
